6cac9ebf18e132275ffa9ccb5b94879beb099bd9
Daniel Ronan <danielconnorronan@gmail.com>
2020-10-15 15:04:02 +0100

    finished validation for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { in: 8..20 }
+  # validates length: { minimum: 8, too_short: "%{count} characters is too short, please enter a minimum of 8" }
 end
