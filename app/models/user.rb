4dc740fd2b2847b2b0a978e084ac7ff35a1056d2
Daniel Ronan <danielconnorronan@gmail.com>
2020-10-16 11:09:19 +0100

    fixed tse requestes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { in: 8..20 }
-  # validates length: { minimum: 8, too_short: "%{count} characters is too short, please enter a minimum of 8" }
 
   has_many :posts
   has_many :comments
--- db/migrate/20201015151151_create_posts.rb
+++
@@ -1,5 +0,0 @@
-class CreatePosts < ActiveRecord::Migration[6.0]
-  def change
-      add_reference :posts, :user
-  end
-end
