91c9872af1b3c2963fa1b9fff4934f20aea27f45
Daniel Ronan <danielconnorronan@gmail.com>
2020-10-16 09:11:40 +0100

    added new migration and created user_id's

---
+++ db/migrate/20201015151151_create_posts.rb
@@ -0,0 +1,5 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+      add_reference :posts, :user
+  end
+end
---
+++ db/migrate/20201016071928_remove_column_from_posts.rb
@@ -0,0 +1,5 @@
+class RemoveColumnFromPosts < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :posts, :users_id
+  end
+end
